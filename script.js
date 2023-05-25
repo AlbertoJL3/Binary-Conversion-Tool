0682657338e67d3a04a1878dac2a9cceb92285d1
AlbertoJL3 <363alberto@gmail.com>
2023-05-25 11:03:22 -0400

    added read functionality for inputs

---
+++ script.js
@@ -0,0 +1,46 @@
+const button = document.querySelector('.btn');
+const resultEl = document.querySelector('.resultsCard');
+const resultsText = document.querySelector('.results');
+
+function getSelectorText() {
+    const menuEl = document.querySelector('.selector');
+    var selectedOption = menuEl.options[menuEl.selectedIndex];
+    var optionText = selectedOption.text;
+    console.log(optionText)
+    return optionText
+
+}
+
+//gets text from input box
+function getInput() {
+    const inputEl = document.querySelector('[name="info"]');
+    var inputValue = inputEl.value;
+    console.log(inputValue)
+    return inputValue
+}
+
+function BinaryToDigit(input) {
+
+}
+
+function DigitToBinary(input) {
+
+}
+
+function BinaryToHexadecimal(input) {
+
+}
+
+function HexadecimalToBinary(input) {
+
+}
+
+function handleEvent() {
+    getSelectorText();
+    getInput();
+}
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    handleEvent();
+});
